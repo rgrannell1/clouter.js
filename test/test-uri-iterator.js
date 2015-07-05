fd2418c62941e0ade96d36a87661cf1e9724b389
rgrannell1 <r.grannell2@gmail.com>
2015-07-05 18:14:17 +0100

    added more tests

--- test/test-uri-iterator.js
+++ test/test-uri-iterator.js
@@ -17,7 +17,7 @@ describe('UriIterator', function ( ) {
 
 	describe('peekRest( )', function ( ) {
 
-		it('returns the entire uri if no segments are removed', function ( ) {
+		it('returns the entire path if no segments are removed', function ( ) {
 
 			;[
 				'/foo/bar',
@@ -25,17 +25,114 @@ describe('UriIterator', function ( ) {
 				'/foo/bar/baz?a=1&b=2',
 				'/foo/bar/baz?a=1#hash'
 			]
-			.forEach(function (uri) {
+			.forEach(function (path) {
 
-				var iter = UriIterator(uri)
+				var iter = UriIterator(path)
 
-				iter.peekRest( ).should.equal(uri)
-				iter.peekRest( ).should.equal(uri)
+				iter.peekRest( ).should.equal(path)
+				iter.peekRest( ).should.equal(path)
 
 			})
 
 		})
 
 	})
+
+	describe('getRest( )', function ( ) {
+
+		it('returns the entire path the first time, and undefined thereafter', function ( ) {
+
+			;[
+				'/foo/bar',
+				'/foo/bar/baz?a=1',
+				'/foo/bar/baz?a=1&b=2',
+				'/foo/bar/baz?a=1#hash'
+			]
+			.forEach(function (path) {
+
+				var iter = UriIterator(path)
+
+				iter.getRest( ).should.equal(path)
+				iter.getRest( ).should.be.undefined
+
+			})
+
+		})
+
+	})
+
+
+
+
+
+	describe('peekNextPaths', function ( ) {
+
+		it('returns all the paths', function () {
+
+			;[
+				['/a',               '/a'],
+				['/foo/bar',         '/foo/bar'],
+				['/foo/bar?a=1&b=2', '/foo/bar'],
+				['/a/b/?q=1',        '/a/b']
+			]
+			.forEach(function (pair) {
+
+				var iter = UriIterator(pair[0])
+
+				iter.peekNextPaths( ).should.equal(pair[1])
+
+				should.not.exist(iter.peekNextPaths( ))
+
+			})
+
+		})
+
+	})
+
+	describe('getNextPaths', function ( ) {
+
+		it('should return all paths, once', function () {
+
+			;[
+				['/a',               '/a'],
+				['/foo/bar',         '/foo/bar'],
+				['/foo/bar?a=1&b=2', '/foo/bar'],
+				['/a/b/?q=1',        '/a/b']
+			]
+			.forEach(function (pair) {
+
+				var iter = UriIterator(pair[0])
+
+				iter.getNextPaths( ).should.equal(pair[1])
+				should.not.exist(iter.getNextPaths( ))
+
+			})
+
+		})
+
+	})
+
+
+
+
+
+	describe('peekNextPath', function ( ) {
+
+		it('', function () {
+
+		})
+
+	})
+
+	describe('getNextPath', function ( ) {
+
+		it('', function () {
+
+		})
+
+	})
+
+
+
 
 })
